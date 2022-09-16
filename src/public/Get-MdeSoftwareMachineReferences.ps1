4e09b1971d5557a5fa0409fd8e54daba2fb52f27
Jan-Henrik Damaschke <jdamaschke@visorian.com>
2022-09-16 11:36:53 +0200

    feat(functions): :sparkles:  Added Get-MdeSoftwareMachineReferences function

---
+++ src/public/Get-MdeSoftwareMachineReferences.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+  Retrieve a list of device references that has this software installed.
+
+.DESCRIPTION
+  Retrieve a list of device references that has this software installed.
+
+.NOTES
+  Author: Jan-Henrik Damaschke
+
+.PARAMETER id
+  Optional. Specifies the id of the target software.
+
+.OUTPUTS
+  PSCustomObject. The Get-MdeSoftwareMachineReferences function returns a list of or a single PSCustomObject containing the parsed MDE machine object.
+
+.EXAMPLE
+  Get-MdeSoftwareMachineReferences -id 'microsoft-_-edge'
+
+.ROLE
+  @(@{permission = 'Software.Read.All'; permissionType = 'Application'}, @{permission = 'Software.Read'; permissionType = 'Delegated'})
+#>
+
+function Get-MdeSoftwareMachineReferences {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
+    [string]
+    $id
+  )
+  Begin {
+    if (-not (Test-MdePermissions -functionName $PSCmdlet.CommandRuntime)) {
+      $requiredRoles = (Get-Help $PSCmdlet.CommandRuntime -Full).role | Invoke-Expression
+      Throw "Missing required permission(s). Please check if one of these is in current token roles: $($requiredRoles.permission)"
+    }
+  }
+  Process {
+    return Invoke-AzureRequest -Uri "https://api.securitycenter.microsoft.com/api/Software/$id/machineReferences"
+  }
+  End {}
+}
