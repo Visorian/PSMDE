5e8aefcb8fabc715f59324e0e206f4896072dc98
prubis <36508582+prubis@users.noreply.github.com>
2023-12-19 11:20:40 +1100

    perf(invokeazurerequest): :zap: removed string concatenation
    Much faster and far less memory usage through sending objects down the 
    pipeline rather than concatting.

--- src/private/Invoke-AzureRequest.ps1
+++ src/private/Invoke-AzureRequest.ps1
@@ -5,12 +5,12 @@ function Invoke-AzureRequest {
     $uri
   )
   $reply = Invoke-RetryRequest -Method Get -Uri $uri
-  $content = $reply.value
+  $reply.value
   while (-not [String]::IsNullOrEmpty($reply.'@odata.nextLink')) {
     Write-Verbose "Found next link: $($reply.'@odata.nextLink')"
     $reply = Invoke-RetryRequest -Method Get -Uri $reply.'@odata.nextLink'
     foreach ($value in $reply.value) {
-      $content += $value
+      $value
     }
   }
   return $content
